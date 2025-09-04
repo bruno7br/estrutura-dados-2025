6b7011dd714588afc7a72013a1f32577cc3dfdef
wagner chagas <wachag_7@hotmail.com>
2025-09-04 16:39:33 -0300

    Atividade Bruno 040925

--- 20250825_comparable/main.java
+++ 20250825_comparable/main.java
@@ -1,19 +1,22 @@
-public class main {
+public class Main {
     public static void main(String[] args) {
-        Automovel auto1 = new Automovel("Marca1", "Modelo1", "Tipo1", "Chassi1", "2020", "2021", "Cor1", "Placa1");
-        ArvoreBinaria<Automovel> arvore = new ArvoreBinaria<>();
-        arvore.inserir(auto1);
-        Automovel auto2 = new Automovel("Marca2", "Modelo1", "Tipo1", "Chassi1", "2020", "2021", "Cor1", "Placa1");
-        Automovel auto3 = new Automovel("Marca3", "Modelo1", "Tipo1", "Chassi1", "2020", "2021", "Cor1", "Placa1");
-        Automovel auto4 = new Automovel("Marca4", "Modelo1", "Tipo1", "Chassi1", "2020", "2021", "Cor1", "Placa1");
-        Automovel auto5 = new Automovel("Marca5", "Modelo1", "Tipo1", "Chassi1", "2020", "2021", "Cor1", "Placa1");
-        Automovel auto6 = new Automovel("Marca6", "Modelo1", "Tipo1", "Chassi1", "2020", "2021", "Cor1", "Placa1");
-        arvore.inserir(auto4);
-        arvore.inserir(auto6);
-        arvore.inserir(auto3);
-        arvore.inserir(auto5);
-        arvore.inserir(auto2);
+        Transporte t1 = new Transporte("Marca1", "Modelo1", "Tipo1", "Chassi1", "2020", "2021", "Cor1", "Placa1");
+        ArvoreBinaria<Transporte> arvore = new ArvoreBinaria<>();
+        arvore.inserir(t1);
+
+        Transporte t2 = new Transporte("Marca2", "Modelo1", "Tipo1", "Chassi2", "2020", "2021", "Cor1", "Placa2");
+        Transporte t3 = new Transporte("Marca3", "Modelo1", "Tipo1", "Chassi3", "2020", "2021", "Cor1", "Placa3");
+        Transporte t4 = new Transporte("Marca4", "Modelo1", "Tipo1", "Chassi4", "2020", "2021", "Cor1", "Placa4");
+        Transporte t5 = new Transporte("Marca5", "Modelo1", "Tipo1", "Chassi5", "2020", "2021", "Cor1", "Placa5");
+        Transporte t6 = new Transporte("Marca6", "Modelo1", "Tipo1", "Chassi6", "2020", "2021", "Cor1", "Placa6");
+
+        arvore.inserir(t4);
+        arvore.inserir(t6);
+        arvore.inserir(t3);
+        arvore.inserir(t5);
+        arvore.inserir(t2);
         arvore.imprimeEmOrdem();
         arvore.imprimePreFixado();
     }
 }
+
